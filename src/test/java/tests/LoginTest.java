1d65dcfa217b50fb97835ab4f683d10eb0277f53
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2023-06-24 08:37:34 +0530

    adding extent repoter for report and listener

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,5 +1,53 @@
 package tests;
 
-public class LoginTest {
+import java.io.IOException;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import pageobjects.HomePage;
+import pageobjects.LoginPage;
+import resources.Base;
+
+public class LoginTest extends Base {
+
+	public WebDriver driver;
+
+	@Test(dataProvider = "getLoginData")
+	public void Login(String username, String password)
+			throws IOException, InterruptedException {
+
+		Thread.sleep(3000);
+
+		LoginPage loginPage = new LoginPage(driver);
+
+		loginPage.usernameField().sendKeys(username);
+		loginPage.passwordField().sendKeys(password);
+		loginPage.loginButton().click();
+
+		HomePage homePage = new HomePage(driver);
+
+		Assert.assertTrue(homePage.loggedUserName().isDisplayed());
+	}
+
+	@BeforeMethod
+	public void openApplication() throws IOException {
+		driver = initializeBrowser();
+		driver.get(prop.getProperty("url"));
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
+	}
+
+	@DataProvider
+	public Object[][] getLoginData() {
+		Object[][] data = { { "vikram", "123" }, { "", "Abc@" },{ "xyz", "" },{"",""} };
+		return data;
+	}
 }
--- src/test/java/tests/TransactionAmountSortingTest.java
+++ src/test/java/tests/TransactionAmountSortingTest.java
@@ -1,5 +1,70 @@
 package tests;
 
-public class TransactionAmountSortingTest {
+import java.io.IOException;
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import pageobjects.HomePage;
+import pageobjects.LoginPage;
+import resources.Base;
+
+public class TransactionAmountSortingTest extends Base {
+
+	public WebDriver driver;
+
+	@Test(dataProvider = "getLoginData")
+	public void AmountSortingTest(String username, String password) throws InterruptedException {
+		
+		Thread.sleep(3000);
+
+		LoginPage loginPage = new LoginPage(driver);
+
+		loginPage.usernameField().sendKeys(username);
+		loginPage.passwordField().sendKeys(password);
+		loginPage.loginButton().click();
+
+		HomePage homePage = new HomePage(driver);
+
+		Thread.sleep(3000);
+		
+		WebElement amountHeader = homePage.getTransactionsTable().findElement(By.id("amount"));
+		amountHeader.click();
+		
+		Thread.sleep(3000);
+		
+		List<Double> transactionAmounts = homePage.getTransactionAmounts();
+		
+		// Check if the transaction amounts are sorted
+		boolean isSorted = homePage.isSorted(transactionAmounts);
+		if (isSorted) {
+		    Assert.assertTrue(true, "Transaction amounts are sorted.");
+		} else {
+		    Assert.fail("Transaction amounts are not sorted.");
+		}
+	}
+
+	@BeforeMethod
+	public void openApplication() throws IOException {
+		driver = initializeBrowser();
+		driver.get(prop.getProperty("url"));
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
+	}
+	
+	@DataProvider
+	public Object[][] getLoginData() {
+		Object[][] data = { { "vikram", "123"}, { "ramu", "456"}};
+		return data;
+	}
 }
